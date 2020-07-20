b84afcbaee8dfd28e9b88945b27d69985dddfa56
Steven C <stevencch99@gmail.com>
2020-07-20 09:56:05 +0800

    Add docker settings

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,20 @@
 class TasksController < ApplicationController
   def index
+    puts 'task_controller#index'
     @task = Task.new
   end
 
   def query
-    @query = Task.where("tasks.id = '#{params[:q]}'").to_sql
-    @tasks = Task.where("tasks.id = '#{params[:q]}'")
-
-    render :index
-  rescue StandardError => e
-    @invalid = true
-    @tasks = nil
-    @query = e
-
+    puts 'task_controller#query'
+    @query = params[:q]
+    @tasks = Task.where("tasks.id = #{params[:q]}")
+    puts '-----------------'
+    puts @query
+    puts @tasks
+    puts '-----------------'
+  rescue e
+    puts e
+  ensure
     render :index
   end
 end
